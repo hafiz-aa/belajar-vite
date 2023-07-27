5487ab96db6a2b3e8b6360487d8db6182fc6f7aa
hafiz-aa <mhafiz.aa@gmail.com>
2023-07-27 13:39:36 +0700

    Build multipage

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,15 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	build: {
 		outDir: 'production',
+		rollupOptions: {
+			input: {
+				index: "index.html",
+				blog: "blog.html",
+				contact: "other/contact.html"
+			}
+		}
 	},
 	server: {
 		port: 3000
 	}
-})
+});
